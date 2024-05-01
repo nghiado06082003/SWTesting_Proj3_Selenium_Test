f2aaa2df4bb585cb35ce6d5ca3c770342927cd08
Huy-DNA <huydo862003@gmail.com>
2024-05-01 20:33:04 +0700

    chore: init test_level_1

---
+++ Selenium_Test_Rust/src/bin/test_level_1/main.rs
@@ -0,0 +1,15 @@
+use std::error::Error;
+use Selenium_Test_Rust::{driver_port::get_driver_port, caps::get_caps};
+use thirtyfour::prelude::*;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let caps = get_caps();
+    let port = get_driver_port();
+    let driver = WebDriver::new(format!("http://localhost:{port}"), caps).await?;
+
+    driver.quit().await?;
+
+    Ok(())
+}
