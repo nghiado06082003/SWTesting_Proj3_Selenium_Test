fa2526e14175050b0c74c12185091c40090e3163
Huy-DNA <huydo862003@gmail.com>
2024-05-01 17:31:09 +0700

    feat: basic structure for level_0 test

--- Selenium_Test_Rust/src/bin/test_level_0/test_suites/test_GPA_calculator.rs
+++ Selenium_Test_Rust/src/bin/test_level_0/test_suites/test_GPA_calculator.rs
@@ -2,6 +2,33 @@ use thirtyfour::prelude::*;
 use std::error::Error;
 
 pub async fn test_GPA_calculator(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("Test GPA calculator suite is running");
     driver.goto("https://www.calculator.net/gpa-calculator.html").await?;
+    test1(driver).await?;
+    test2(driver).await?;
+    test3(driver).await?;
+    test4(driver).await?;
     Ok(())
 }
+
+pub async fn test1(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("Test1 is running!");
+    Ok(())
+}
+
+pub async fn test2(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("Test2 is running!");
+    Ok(())
+}
+
+pub async fn test3(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("Test3 is running!");
+    Ok(())
+}
+
+pub async fn test4(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("Test4 is running!");
+    Ok(())
+    
+}
+
