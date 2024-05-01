3c1c4106fb21c82fd1efca51068350bc8d1ab5d2
Huy-DNA <huydo862003@gmail.com>
2024-05-01 17:33:20 +0700

    refactor: tests level 0

--- Selenium_Test_Rust/src/bin/test_level_0/test_suites/test_GPA_calculator.rs
+++ Selenium_Test_Rust/src/bin/test_level_0/test_suites/test_GPA_calculator.rs
@@ -1,9 +1,10 @@
 use thirtyfour::prelude::*;
 use std::error::Error;
 
+static URL: &'static str = "https://www.calculator.net/gpa-calculator.html";
+
 pub async fn test_GPA_calculator(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
     println!("Test GPA calculator suite is running");
-    driver.goto("https://www.calculator.net/gpa-calculator.html").await?;
     test1(driver).await?;
     test2(driver).await?;
     test3(driver).await?;
@@ -13,21 +14,25 @@ pub async fn test_GPA_calculator(driver: &WebDriver) -> Result<(), Box<dyn Error
 
 pub async fn test1(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
     println!("Test1 is running!");
+    driver.goto(URL).await?;
     Ok(())
 }
 
 pub async fn test2(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
     println!("Test2 is running!");
+    driver.goto(URL).await?;
     Ok(())
 }
 
 pub async fn test3(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
     println!("Test3 is running!");
+    driver.goto(URL).await?;
     Ok(())
 }
 
 pub async fn test4(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>> {
     println!("Test4 is running!");
+    driver.goto(URL).await?;
     Ok(())
     
 }
